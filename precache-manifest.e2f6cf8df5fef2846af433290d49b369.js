69681fca6d94cfb9b96e49ce71ca2bbc98580e2a
Sir Oliver <siroliver3006@gmail.com>
2020-06-08 09:01:03 +0100

    Updates

---
+++ precache-manifest.e2f6cf8df5fef2846af433290d49b369.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea4a22781b8f63d765e22801e3a18c8d",
+    "url": "/NEURON/index.html"
+  },
+  {
+    "revision": "897d1137f17b34191cac",
+    "url": "/NEURON/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8c086823aa22e3481ab8",
+    "url": "/NEURON/static/js/2.bbc67504.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/NEURON/static/js/2.bbc67504.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "897d1137f17b34191cac",
+    "url": "/NEURON/static/js/main.33f90aec.chunk.js"
+  },
+  {
+    "revision": "36fbcaa3ea8bee860f45",
+    "url": "/NEURON/static/js/runtime-main.5c849184.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/NEURON/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/neuron/precache-manifest.84284d6ae14164f2e04c67a1190bc2db.js"
+  "/NEURON/precache-manifest.e2f6cf8df5fef2846af433290d49b369.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/neuron/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/NEURON/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });

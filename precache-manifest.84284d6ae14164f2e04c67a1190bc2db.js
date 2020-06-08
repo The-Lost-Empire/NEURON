2cac2fe471c5f8f70564638788bc2726bfe386f1
Sir Oliver <siroliver3006@gmail.com>
2020-06-08 09:00:00 +0100

    Updates

---
+++ precache-manifest.84284d6ae14164f2e04c67a1190bc2db.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64d0aded666468298ccfa1134a1596ea",
+    "url": "/neuron/index.html"
+  },
+  {
+    "revision": "281fd2e523cd40eee4eb",
+    "url": "/neuron/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8c086823aa22e3481ab8",
+    "url": "/neuron/static/js/2.bbc67504.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/neuron/static/js/2.bbc67504.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "281fd2e523cd40eee4eb",
+    "url": "/neuron/static/js/main.86f8532d.chunk.js"
+  },
+  {
+    "revision": "0ffc8f3f00f4205c9653",
+    "url": "/neuron/static/js/runtime-main.53512c48.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/neuron/static/media/logo.5d5d9eef.svg"
+  }
+]);
